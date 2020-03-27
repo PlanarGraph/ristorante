1df30496ffcf1008ea2fb7e870bc84cc1ebc7c7e
Nolan Tiffney <nolantiffney@gmail.com>
2020-03-27 02:29:22 -0400

    Add image path to dishes db.

--- apps/ristorante/lib/ristorante/food/dish.ex
+++ apps/ristorante/lib/ristorante/food/dish.ex
@@ -6,6 +6,7 @@ defmodule Ristorante.Food.Dish do
     field :name, :string
     field :price, :float
     field :description, :string
+    field :image_path, :string
 
     belongs_to :category, Ristorante.Food.Category
     timestamps()
@@ -18,5 +19,28 @@ defmodule Ristorante.Food.Dish do
     |> validate_required([:name, :price])
     |> assoc_constraint(:category)
     |> unique_constraint(:name)
+    |> add_image_path()
+  end
+
+  defp add_image_path(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{name: name, category_id: cid}} ->
+        category = Ristorante.Food.get_category_by(id: cid)
+        put_change(changeset, :image_path, gen_image_path(name, category))
+
+      _ ->
+        IO.puts("here")
+        changeset
+    end
+  end
+
+  defp gen_image_path(name, category) do
+    name_format =
+      name
+      |> String.downcase()
+      |> String.split(" ")
+      |> Enum.join()
+
+    "/images/" <> category.name <> "/" <> name_format <> ".jpg"
   end
 end
