06694eaf4bfc09c0ca357478305d5aa17f415617
Nolan Tiffney <nolantiffney@gmail.com>
2020-03-29 23:23:31 -0400

    authenticate_user/2 redirects to login on fail.

--- apps/ristorante_web/lib/ristorante_web/controllers/auth.ex
+++ apps/ristorante_web/lib/ristorante_web/controllers/auth.ex
@@ -42,7 +42,7 @@ defmodule RistoranteWeb.Auth do
     else
       conn
       |> put_flash(:error, "You must be logged in to access that page.")
-      |> redirect(to: Routes.page_path(conn, :index))
+      |> redirect(to: Routes.session_path(conn, :new))
       |> halt()
     end
   end
